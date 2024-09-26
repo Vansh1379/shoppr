002c84d576a695907434c04cfd0e96b061888521
vansh kalra <vanshkalra1379@gmail.com>
2024-09-26 16:25:57 +0530

    Added login route

--- Backend/src/controllers/userAuth.ts
+++ Backend/src/controllers/userAuth.ts
@@ -10,9 +10,11 @@ const prisma = new PrismaClient();
 export const singupAuth = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
+
         //zod
         const createPayload = req.body;
         const parsePayload = signupValidation.safeParse(createPayload);
+
         if (!parsePayload.success) {
             return res.status(404).json({
                 msg: "Invalid Input send by the bitch",
@@ -22,8 +24,9 @@ export const singupAuth = async (req: Request, res: Response, next: NextFunction
 
         //checking wheather user already exist or not 
         const userExist = await prisma.user.findFirst({
-            where: { email: req.body.email }
+            where: { email: createPayload.email }
         });
+
         if (userExist) {
             res.status(409).json({
                 msg: "User alreagy eaxist Please login bitch",
@@ -61,12 +64,35 @@ export const singupAuth = async (req: Request, res: Response, next: NextFunction
 // .......................................................................................................................................................
 export const loginLogic = async (req: Request, res: Response, nest: NextFunction) => {
     try {
+
+        //zod 
         const createPayload = req.body;
         const parsePayload = loginValidation.safeParse(createPayload);
+
         if (!parsePayload.success) {
             return res.status(404).json({
-                msg: "Invalid Inout by the user bitch"
+                msg: "Invalid Inout by the user bitch",
+                errors: parsePayload.error.errors
             })
+        }
+
+        //checking wheather user exist or not in db
+        const userExists = await prisma.user.findFirst({
+            where: {
+                email: createPayload.email,
+                password: createPayload.password
+            }
+        });
+
+        if (userExists) {
+            const token = jwt.sign({
+                data: userExists.id
+            }, "secret", { expiresIn: '5hr' });
+
+            res.status(200).json({
+                msg: 'User have logged in succesfully',
+                token
+            });
         }
     }
     catch (err) {
@@ -79,3 +105,5 @@ export const loginLogic = async (req: Request, res: Response, nest: NextFunction
         console.log("User Loged in Successfully")
     }
 }
+
+//................................................................................................
--- Backend/src/routes/userRouter.ts
+++ Backend/src/routes/userRouter.ts
@@ -4,7 +4,7 @@ import { loginLogic, singupAuth } from "../controllers/userAuth";
 const router = Router();
 
 router.post("/signup", singupAuth);
-router.post("login", loginLogic);
+router.post("/login", loginLogic);
 
 
 export default router;
