341953d0e2c1c143b6c257bdc577a0508fd6b1a1
vansh kalra <vanshkalra1379@gmail.com>
2024-09-30 23:34:46 +0530

    Frontend

---
+++ Frontend-2/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-200">
+      <h1 className="text-4xl text-gray-800 font-bold">Hello, Tailwind CSS with React and TypeScript!</h1>
+    </div>
+  )
+}
+
+export default App
