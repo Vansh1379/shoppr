bc6457ae626f38d0f1d500cd316f2152ae5f66ba
vansh kalra <vanshkalra1379@gmail.com>
2024-10-02 19:27:42 +0530

    Added landing compo

---
+++ Frontend-2/src/components/LandingCompo.tsx
@@ -0,0 +1,31 @@
+import img1 from "../assets/img1.png"
+import img2 from "../assets/img2.png"
+import img3 from "../assets/img3.png"
+import discount from "../assets/Discount.png"
+import { useNavigate } from "react-router-dom"
+import navratre from "../assets/navratre.png"
+
+export const LandingCompo = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="bg-gray-100 max-w-screen overflow-hidden">
+            <div className="flex items-center justify-evenly px-10 py-9 rounded-lg">
+                <div >
+                    <img src={img1} alt="image1" />
+                </div>
+                <div>
+                    <img src={img2} alt="image2" />
+                </div>
+                <div>
+                    <img src={img3} alt="image3" />
+                </div>
+            </div>
+            <div>
+                <img src={discount} alt="discount" />
+            </div>
+            <div>
+                <img src={navratre} alt="navrate sale" />
+            </div>
+        </div>
+    )
+}
--- Frontend-2/src/pages/LandingPage.tsx
+++ Frontend-2/src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
+import { LandingCompo } from '../components/LandingCompo'
 import { Navbar } from '../components/Navbar'
 import { RunningDiv } from '../components/RunningDiv'
 
 export const LandingPage = () => {
     return (
-        <>
+        <div className='overflow-hidden'>
             <Navbar />
             <RunningDiv />
-        </>
+            <LandingCompo/>
+        </div>
     )
 }
