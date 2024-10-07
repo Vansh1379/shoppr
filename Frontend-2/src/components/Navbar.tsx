666b829088280d6e7ae1fbfdd4878fcd90c0eafc
vansh kalra <vanshkalra1379@gmail.com>
2024-10-07 07:21:38 +0530

    Added Some changes

--- Frontend-2/src/components/Navbar.tsx
+++ Frontend-2/src/components/Navbar.tsx
@@ -33,10 +33,12 @@ export const Navbar: React.FC = () => {
                         <div className="text-base text-black font-semibold  m-0 p-8 hover:text-pink-500">Beauty Advice</div>
                     </div>
                     <div className="flex items-center bg-gray-100  pl-2  rounded-md ml-44">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-7 pt-1 mr-2 border-non hover:text-pink-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            className="size-7 pt-1 mr-2 border-non hover:text-pink-700">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
-                        <input type="search" id="search" name="search" placeholder="Type here to search..." className="bg-gray-100 pl-3 pr-16 py-2 border-none hover:bg-gray-200 text-pink-500" />
+                        <input type="search" id="search" name="search" placeholder="Type here to search..."
+                            className="bg-gray-100 pl-3 pr-16 py-2 border-none hover:bg-gray-200 text-pink-500" />
                     </div>
                 </div>
 
