61b7a3126c4ca240579e980db42c6e6cae3fa9a4
vansh kalra <vanshkalra1379@gmail.com>
2024-10-09 00:34:41 +0530

    Changed the navbar

--- Frontend-2/src/pages/LandingPage.tsx
+++ Frontend-2/src/pages/LandingPage.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from 'react'
 import { Fotter } from '../components/Fotter'
 import { LandingCompo } from '../components/LandingCompo'
 import { LandingProduct } from '../components/LandingProduct'
 import { Navbar } from '../components/Navbar'
-
+import { LoginNavbar } from '../components/LoginNavbar'
 
 export const LandingPage = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, []);
+
     return (
         <div className='overflow-hidden'>
-            <Navbar />
+            {isLoggedIn ? <LoginNavbar /> : <Navbar />}
             <LandingCompo />
             <LandingProduct />
             <Fotter />
